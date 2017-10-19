7afae59194a041fb0409757947979b52ef7ebc5a
Ivan Premuzic <ivan.premuzic@gmail.com>
2017-10-19 22:44:52 +0200

    Četvrti zadatak

---
+++ Assignment4/Ball.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Assignment4
+{
+    /// <summary >
+    /// Game ball object representation
+    /// </ summary >
+    public class Ball : Sprite
+    {
+        /// <summary >
+        /// Defines current ball speed in time .
+        /// </ summary >
+        public float Speed { get; set; }
+
+        public float BumpSpeedIncreaseFactor { get; set; }
+        public float MaximumSpeed { get; set; }
+       
+        /// <summary >
+        /// Defines ball direction .
+        /// Valid values ( -1 , -1) , (1 ,1) , (1 , -1) , ( -1 ,1).
+        /// Using Vector2 to simplify game calculation . Potentially
+        /// dangerous because vector 2 can swallow other values as well .
+        /// OPTIONAL TODO : create your own , more suitable type
+        /// </ summary >
+        public Vector2 Direction { get; set; }
+
+        public Ball(int size, float speed, float
+            defaultBallBumpSpeedIncreaseFactor, float defaultMaximumSpeed) : base(size, size)
+        {
+            Speed = speed;
+            BumpSpeedIncreaseFactor = defaultBallBumpSpeedIncreaseFactor;
+            // Initial direction
+            Direction = new Vector2(1, 1);
+            MaximumSpeed = defaultMaximumSpeed;
+        }
+    }
+}
---
+++ Assignment4/CollisionDetector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class CollisionDetector
+    {
+        public static bool Overlaps(IPhysicalObject2D a, IPhysicalObject2D b)
+        { 
+          //TODO  
+            return false;
+        }
+
+    }
+}
